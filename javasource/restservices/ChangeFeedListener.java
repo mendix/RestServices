b344f38c8916cac6fc68adf3af38d1437443fc08
mweststrate <mweststrate@gmail.com>
2013-12-20 23:51:44 +0000

    more on syncing
    git-svn-id: https://teamserver.sprintr.com/2f1dfdec-5e3d-497c-9706-741732a51cf2/trunk@35 5f19bcb0-64dc-40a8-b24f-70cbb7b2b6a4

--- javasource/restservices/ChangeFeedListener.java
+++ javasource/restservices/ChangeFeedListener.java
@@ -40,14 +40,19 @@ public class ChangeFeedListener {
 		activeListeners.put(url, this);
 		this.state = XPath.create(Core.createSystemContext(), CollectionFollowState.class).findOrCreate(CollectionFollowState.MemberNames.CollectionUrl, url);
 		
-		String requestUrl = collectionUrl + "?feed=true&since=" + state.getRevision() + 1; //revision expresses the last *received* revision, so start +1. 
+		restartConnection();
+	}
+
+	private void restartConnection() throws IOException,
+			HttpException {
+		String requestUrl = url + "?feed=true&since=" + state.getRevision() + 1; //revision expresses the last *received* revision, so start +1. 
 		
 		GetMethod get = new GetMethod(requestUrl);
 		get.setRequestHeader(Constants.ACCEPT_HEADER, Constants.TEXTJSON);
 		
 		int status = Consumer.client.executeMethod(get);
 		if (status != IMxRuntimeResponse.OK)
-			throw new RuntimeException("Failed to setup stream to " + collectionUrl +  ", status: " + status);
+			throw new RuntimeException("Failed to setup stream to " + url +  ", status: " + status);
 		
 		this.inputStream = get.getResponseBodyAsStream();
 
@@ -66,33 +71,46 @@ public class ChangeFeedListener {
 	void listen()
 			throws CoreException, Exception {
 		JSONTokener jt = new JSONTokener(inputStream);
-		JSONObject instr;
-		while(null != (instr = new JSONObject(jt))) {
-			IContext c = Core.createSystemContext();
-			//TODO: use constants
-			//TODO: store revision
-			if (instr.getBoolean("deleted")) {
-				Core.execute(c, onDeleteMF, instr.getString("key"));
+		JSONObject instr = null;
+		
+		boolean graceFullDisconnected = true;
+		
+		try {
+			while(true) {
+				instr = new JSONObject(jt);
+				
+				IContext c = Core.createSystemContext();
+				//TODO: use constants
+				//TODO: store revision
+				if (instr.getBoolean("deleted")) {
+					Core.execute(c, onDeleteMF, instr.getString("key"));
+				}
+				else {
+					IDataType type = Utils.getFirstArgumentType(onUpdateMF);
+					if (!type.isMendixObject())
+						throw new RuntimeException("First argument should be an Entity! " + onUpdateMF);
+	
+					IMendixObject target = Core.instantiate(c, type.getObjectType());
+					Consumer.readJsonObjectIntoMendixObject(c, instr.getJSONObject("data"), target, new ObjectCache());
+					Core.commit(c, target);
+					Core.execute(c, onUpdateMF, target);
+				}
+				
+				long revision = instr.getLong("rev"); //TODO: doublecheck this context remains valid..
+				
+				if (revision <= state.getRevision()) 
+					RestServices.LOG.warn("Received revision (" + revision + ") is smaller as latest known revision (" + state.getRevision() +"), probably the collections are out of sync?");
+				
+				state.setRevision(revision);
+				state.commit();
 			}
-			else {
-				IDataType type = Utils.getFirstArgumentType(onUpdateMF);
-				if (!type.isMendixObject())
-					throw new RuntimeException("First argument should be an Entity! " + onUpdateMF);
-
-				IMendixObject target = Core.instantiate(c, type.getObjectType());
-				Consumer.readJsonObjectIntoMendixObject(c, instr.getJSONObject("data"), target, new ObjectCache());
-				Core.commit(c, target);
-				Core.execute(c, onUpdateMF, target);
-			}
-			
-			long revision = instr.getLong("rev"); //TODO: doublecheck this context remains valid..
-			
-			if (revision <= state.getRevision()) 
-				RestServices.LOG.warn("Received revision (" + revision + ") is smaller as latest known revision (" + state.getRevision() +"), probably the collections are out of sync?");
-			
-			state.setRevision(revision);
-			state.commit();
 		}
+		catch(Exception e) {
+			graceFullDisconnected = false; //TODO unless..
+		}
+		
+		if (graceFullDisconnected)
+			restartConnection();
 	}
 	
 	private void close() throws IOException {
