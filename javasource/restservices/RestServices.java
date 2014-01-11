1897e985a16f21606bd967f36a9888d07df42144
mweststrate <mweststrate@gmail.com>
2014-01-11 23:42:53 +0000

    batch size should not be final
    git-svn-id: https://teamserver.sprintr.com/2f1dfdec-5e3d-497c-9706-741732a51cf2/trunk@75 5f19bcb0-64dc-40a8-b24f-70cbb7b2b6a4

--- javasource/restservices/RestServices.java
+++ javasource/restservices/RestServices.java
@@ -13,9 +13,9 @@ import com.mendix.systemwideinterfaces.core.meta.IMetaObject;
 
 public class RestServices {
 	
-	public static final long BATCHSIZE = 1000;
+	public static int BATCHSIZE = 1000;
 
-	public static final ILogNode LOG = Core.getLogger("RestPublisher");
+	public static ILogNode LOG = Core.getLogger("RestPublisher");
 	
 	public static final String VERSION = "1.0.0";
 	
