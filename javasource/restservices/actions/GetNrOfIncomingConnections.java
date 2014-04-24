85789d3a5c9e7cff5c78bae45cfd524edef820c4
michel <michel@ws019.mendixdomain.local>
2014-04-24 15:24:08 +0200

    Autogenerated but altered whitespace :-/

--- javasource/restservices/actions/GetNrOfIncomingConnections.java
+++ javasource/restservices/actions/GetNrOfIncomingConnections.java
@@ -12,7 +12,6 @@ package restservices.actions;
 import restservices.RestServices;
 import restservices.proxies.ServiceDefinition;
 import restservices.publish.PublishedService;
-
 import com.mendix.systemwideinterfaces.core.IMendixObject;
 import com.mendix.systemwideinterfaces.core.IContext;
 import com.mendix.webui.CustomJavaAction;
