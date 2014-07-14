ce65f34b4c5b6fe11769ca92c80e8e0208aaced2
Michel Weststrate <michel.weststrate@mendix.com>
2014-07-14 15:30:38 +0200

    Introduced workaround for serialization bug caused by Core.retrieveIdList (see support ticket 102188)

--- javasource/tests/SerializationTests.java
+++ javasource/tests/SerializationTests.java
@@ -5,10 +5,12 @@ import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 
+import restservices.proxies.BooleanValue;
+import restservices.util.Function;
 import restservices.util.JsonDeserializer;
 import restservices.util.JsonSerializer;
+import restservices.util.Utils;
 import tests.proxies.A;
-import restservices.proxies.BooleanValue;
 import tests.proxies.CustomBooleanTest;
 import tests.proxies.Task;
 
@@ -33,9 +35,9 @@ public class SerializationTests extends TestBase {
 	@Test
 	public void testDeserialize1() throws Exception {
 		
-		IContext c = Core.createSystemContext();
+		final IContext c = Core.createSystemContext();
 		
-		JSONObject data = new JSONObject();
+		final JSONObject data = new JSONObject();
 		
 		data.put("_ID", "17");
 		data.put("super_name", "34");
@@ -56,23 +58,32 @@ public class SerializationTests extends TestBase {
 		data.put("A_B", b1);
 		data.put("_A_bs", ar);
 
-		A a = new A(c);
-		JsonDeserializer.readJsonDataIntoMendixObject(c, data, a.getMendixObject(), false);
+		Utils.withSessionCache(c, new Function<Boolean>() {
+
+			@Override
+			public Boolean apply() throws Exception {
+				A a = new A(c);
+			
+				JsonDeserializer.readJsonDataIntoMendixObject(c, data, a.getMendixObject(), false);
 		
-		Assert.assertEquals("17", a.get_id());
-		Assert.assertEquals("34", a.getsuper_name());
-		Assert.assertEquals("test1", a.getA_B().getattr());
-		Assert.assertEquals(2, a.get_A_Bs().size());
-		Assert.assertEquals("test2", a.get_A_Bs().get(0).getattr());
-		Assert.assertEquals("test3", a.get_A_Bs().get(1).getattr());
+				Assert.assertEquals("17", a.get_id());
+				Assert.assertEquals("test1", a.getA_B().getattr());
+				Assert.assertEquals(2, a.get_A_Bs().size());
+				Assert.assertEquals("test2", a.get_A_Bs().get(0).getattr());
+				Assert.assertEquals("test3", a.get_A_Bs().get(1).getattr());
+				return true;
+			}
+			
+		});
+		
 	}
 	
 	@Test
 	public void testDeserialize2() throws Exception {
 		
-		IContext c = Core.createSystemContext();
+		final IContext c = Core.createSystemContext();
 		
-		JSONObject data = new JSONObject();
+		final JSONObject data = new JSONObject();
 		
 		data.put("ID", "17");
 		data.put("super-name", "34");
@@ -93,15 +104,22 @@ public class SerializationTests extends TestBase {
 		data.put("a_B", b1);
 		data.put("a$bs", ar);
 
-		A a = new A(c);
-		JsonDeserializer.readJsonDataIntoMendixObject(c, data, a.getMendixObject(), false);
-		
-		Assert.assertEquals("17", a.get_id());
-		Assert.assertEquals("34", a.getsuper_name());
-		Assert.assertEquals("test1", a.getA_B().getattr());
-		Assert.assertEquals(2, a.get_A_Bs().size());
-		Assert.assertEquals("test2", a.get_A_Bs().get(0).getattr());
-		Assert.assertEquals("test3", a.get_A_Bs().get(1).getattr());
+		Utils.withSessionCache(c, new Function<Boolean>() {
+
+			@Override
+			public Boolean apply() throws Exception {
+				A a = new A(c);
+				JsonDeserializer.readJsonDataIntoMendixObject(c, data, a.getMendixObject(), false);
+				
+				Assert.assertEquals("17", a.get_id());
+				Assert.assertEquals("34", a.getsuper_name());
+				Assert.assertEquals("test1", a.getA_B().getattr());
+				Assert.assertEquals(2, a.get_A_Bs().size());
+				Assert.assertEquals("test2", a.get_A_Bs().get(0).getattr());
+				Assert.assertEquals("test3", a.get_A_Bs().get(1).getattr());
+				return true;
+			}
+		});
 	}
 	
 	@Test
